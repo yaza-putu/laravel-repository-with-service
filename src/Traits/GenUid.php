e4266f0f8be6b53153b49cbab177ab6c80313ef0
malika <wayan13yasa@gmail.com>
2021-11-27 12:51:31 +0800

    add genuid

---
+++ src/Traits/GenUid.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaravelEasyRepository\Mutators;
+
+
+trait GenUid
+{
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = uid();
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
